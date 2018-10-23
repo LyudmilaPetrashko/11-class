5d0ce6d7f74577a9383cbc502f0b6fd4ba0b6365
Mila <32988112+LyudmilaPetrashko@users.noreply.github.com>
2018-10-23 22:59:46 +0300

    Update find_bridges.cpp

--- find_bridges.cpp
+++ find_bridges.cpp
@@ -1,115 +1,101 @@
-#include<iostream>
-#include<vector>
+//поиск мостов
+#include <iostream>
+#include <vector>
+#include<utility>
+
 using namespace std;
 
-int v;
-
-vector<vector<int> >  g;
-
-
-bool* used= new bool[v];
+int v; 
+bool* used = new bool[v];
 int timer;
-int* tin=new int[v];
-int* fup=new int[v];
+int* t_in = new int[v];
+int* f_up = new int[v];
+int** g = new int* [v];
 
-
- void IS_BRIDGE(int a, int b){
-    cout<<endl<<"("<<a<<","<<b<<")";
+void bridge(int a, int b){
+    cout<<endl<<"("<<a+1<<","<<b+1<<")";
 }
 
 
-void dfs (int k, int p = -1) {
-	used[k] = true;
-	tin[k] = fup[k] = timer++;
-	for (size_t i=0; i<g[k].size(); ++i) {
-		int to = g[k][i];
-		if (to == p)  continue;
-		if (used[to])
-			fup[k] = min (fup[k], tin[to]);
-		else {
-			dfs (to, k);
-			fup[k] = min (fup[k], fup[to]);
-			if (fup[to] > tin[k])
-				IS_BRIDGE(k+1,to+1);
-		}
+void dfs(int k, int p=-1){
+    used[k]=1;
+    t_in[k]=f_up[k]=timer++;
+    for (int i=0; i<v; i++){
+        if (g[k][i]!=0){
+            int to=i;
+            if (to==p) continue;
+            if (used[to])
+                f_up[k]=min(f_up[k], t_in[to]);
+            else{
+                dfs(to, k);
+                f_up[k]=min(f_up[k], f_up[to]);
+                if (f_up[to]>t_in[k])
+                    bridge(k, to);
+            }
+        }
+    }
+}
+
+void find_bridges() {
+	timer=0;
+	for (int i=0; i<v; i++){
+		used[i] = 0;
+	}
+	for(int i=0; i<v; i++){
+		if(!used[i])
+			dfs(i);
 	}
 }
 
-
-
-
-void find_bridges() {
-	timer = 0;
-	for (int i=0; i<v; ++i)
-		used[i] = false;
-	for (int i=0; i<v; ++i)
-		if (!used[i])
-			dfs (i);
-}
-
-
-
-void make_list_con(vector<pair<int,int> > a, vector<vector<int> > &b, int k){//1-данная вершина, 2-те,что связаны с ней ребром
-    vector<int> temp;
-    for (int j = 0; j < k; j++){
-        b.push_back(temp);
-    }
-    for(int i=0; i<a.size(); i++){
-        b[a[i].first-1].push_back(a[i].second);
-        b[a[i].second-1].push_back(a[i].first);
-    }
-}
-
-void print_list_con(vector<vector<int> >z, int l){
-    cout<<endl<<"the list of connectivity: "<<endl;
-    for (int i=0; i<l; i++)
-    {
-        cout <<i+1<< ") ";
-        for (int j = 0; j<z[i].size(); j++){
-            cout << z[i][j]<<" ";
+void make_matrix_adj(vector<pair<int,int> > gr1, int** matr){
+        for(int k=0; k<gr1.size(); k++){
+            matr[gr1[k].first-1][gr1[k].second-1]=1;
+            matr[gr1[k].second-1][gr1[k].first-1]=1;
         }
-        cout << endl;
 
-    }
-}
 
+  }
 
 int main(){
-
-
     int E;
     cout<<"print number of vertices: ";
     cin>>v;
     cout<<"print number of edges: ";
     cin>>E;
 
-    int* used = new int[v];// дополнительный массив
-    for(int j=0; j<v; j++){
-        used[j]=0;
-    }
-
 
     //список ребер
     vector<pair<int,int> > h;
 
+    bool* us=new bool[v];
+
     //заполнение списка
     int f,s;
     for(int i=0; i<E; i++){
-        cout<<"enter data for the "<<i+1<<" edge: ";
-        cin>>f>>s;
+        cout<<"enter data for the "<<i+1<<" edge "<<endl<<"beginning: ";
+        cin>>f;
+        cout<<"end: ";
+        cin>>s;
         h.push_back(make_pair(f,s));
+        cout<<endl;
     }
 
+    //матрица смежностей
+    for(int i=0; i<v; i++){
+        g[i]=new int[v];
+        us[i]=0;
+    }
 
+    for(int i=0; i<v; i++){
+        for(int j=0; j<v; j++){
+            g[i][j]=0;
+        }
+    }
 
+    make_matrix_adj( h, g);
+    
+    cout<<"bridges: ";
+	find_bridges();
 
-
-    make_list_con(h,g,v);
-/////////////////////////////////////////////////////////////////////////////
-
-    find_bridges();
-
-    delete[] used;
-
-    return 0;
+	return 0;
 }
